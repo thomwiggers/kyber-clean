59e55dacbdadda5e33a54d84f1cab4f4b1ead97e
Thom Wiggers <thom@thomwiggers.nl>
2019-09-17 12:58:26 +0200

    First attempt at integrating AVX2 90s

--- avx2/symmetric.h
+++ avx2/symmetric.h
@@ -6,18 +6,18 @@
 #ifdef KYBER_90S
 
 #include "aes256ctr.h"
-#include <openssl/sha.h>
+#include "sha2.h"
 
 #if (KYBER_SSBYTES != 32)
 #error "90s variant of Kyber can only generate keys of length 256 bits"
 #endif
 
-#define hash_h(OUT, IN, INBYTES) SHA256(IN, INBYTES, OUT)
-#define hash_g(OUT, IN, INBYTES) SHA512(IN, INBYTES, OUT)
+#define hash_h(OUT, IN, INBYTES) sha256(OUT, IN, INBYTES)
+#define hash_g(OUT, IN, INBYTES) sha512(OUT, IN, INBYTES)
 #define xof_absorb(STATE, IN, X, Y) PQCLEAN_NAMESPACE_aes256ctr_init(STATE, IN, Y + ((uint16_t)X << 8))
 #define xof_squeezeblocks(OUT, OUTBLOCKS, STATE) PQCLEAN_NAMESPACE_aes256ctr_squeezeblocks(OUT, OUTBLOCKS, STATE)
 #define prf(OUT, OUTBYTES, KEY, NONCE) PQCLEAN_NAMESPACE_aes256ctr_prf(OUT, OUTBYTES, KEY, NONCE)
-#define kdf(OUT, IN, INBYTES) SHA256(IN, INBYTES, OUT)
+#define kdf(OUT, IN, INBYTES) sha256(OUT, IN, INBYTES)
 
 #define XOF_BLOCKBYTES 128
 
